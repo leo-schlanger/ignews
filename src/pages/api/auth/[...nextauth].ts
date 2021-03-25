6f6631ae262d30910ed00d98c8ee221ca112831c
leo-schlanger <leoschlanger@gmail.com>
2021-03-25 15:07:49 -0300

    stripe payment

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -16,7 +16,6 @@ export default NextAuth({
   callbacks: {
     async signIn(user, account, profile) {
       const { email } = user;
-      console.log(user)
 
       try {
         await fauna.query(
